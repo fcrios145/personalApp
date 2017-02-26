857817cc3c09845580dd6dd849073b1508b4d29e
franciscoRios <fcrios145@hotmail.com>
2017-02-25 17:16:05 -0700

    order code

--- personal_app/personal_app/settings.py
+++ personal_app/personal_app/settings.py
@@ -141,7 +141,11 @@ STATIC_URL = '/static/'
 
 OAUTH2_PROVIDER = {
     # this is the list of available scopes
-    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
+    'SCOPES': {
+        'read': 'Read scope',
+        'write': 'Write scope', 
+        'groups': 'Access to your groups'
+    }
 }
 
 REST_FRAMEWORK = {
